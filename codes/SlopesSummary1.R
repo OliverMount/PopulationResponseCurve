a73af5aa70ec6394e0c26fb61b4e790d7466e471
OliverMount <oliverjmount@yahoo.com>
2024-04-23 07:28:44 +0900

    updating

--- codes/SlopesSummary1.R
+++ codes/SlopesSummary1.R
@@ -71,7 +71,7 @@ df_passive$paradigm<- rep("passive",nrow(df_passive))
 df<- rbind(df_task,df_passive)
 
 df_new <-  df %>% pivot_longer(cols = c("Homo","Hetero"), 
-                               values_to = "slopes")
+                               values_to = "slopes") 
 
 write.csv(df_new ,file=paste0(save_path,'significant.csv')) 
 
@@ -79,12 +79,25 @@ write.csv(df_new ,file=paste0(save_path,'significant.csv'))
 df_new$Condition<- factor(df_new$Condition,levels = c("V1_45","V1_90","V1_135","PPC_45","PPC_90","PPC_135"),
                       labels = c("V1 45","V1 90","V1 135","PPC 45","PPC 90","PPC 135"),
                       ordered = TRUE)
+df_new$paradigm<- factor(df_new$paradigm,levels = c("task","passive"),
+                     labels =  c("task","passive"),
+                     ordered = TRUE)
 df_new$name<- factor(df_new$name,levels = c("Homo","Hetero"),
                      labels =  c("Homo","Hetero"),
                      ordered = TRUE)
-
-
 df_new$Percent<- factor(df_new$Percent,levels = c("0","10","20","40","60","100"))
+
+
+
+# all
+df_all <- df_new %>%
+  group_by(Condition, Percent) %>%
+  summarize(mean_value = mean(slopes),
+            sd_value = sd(slopes),
+            se_value = sd_value / sqrt(n()))
+write.csv(df_all ,file=paste0(save_path,'all.csv')) 
+
+
 
 #task passive
 df_task_passive <- df_new %>%
@@ -104,9 +117,6 @@ df_ho_het <- df_new %>%
 
 
 write.csv(df_ho_het ,file=paste0(save_path,'homo_hetero.csv')) 
-
-
-
 
 
 
